37210aa68c471332488e75a4151ed6d624f9aea2
ahmed khan <ahmedkhan@ahmeds-MacBook-Air.local>
2026-06-06 17:01:46 +0530

    adding speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,29 @@
 //   </StrictMode>,
 // );
 
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { inject } from '@vercel/analytics'  // ADD THIS
+// import './index.css'
+// import App from './App'
+
+// inject()  // ADD THIS
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'  // ADD THIS
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import './index.css'
 import App from './App'
 
-inject()  // ADD THIS
+inject()
+injectSpeedInsights()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
